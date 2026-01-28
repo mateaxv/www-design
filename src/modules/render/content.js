75d8ca902c5210f92bd6540c0585e09e5f38189f
mateaxv <matea.varvodic@gmail.com>
2026-01-28 13:38:40 +0100

    contents

--- src/modules/render/content.js
+++ src/modules/render/content.js
@@ -1,11 +1,5 @@
 export const content = [
-  { file: './content/.DS_Store', lastModified: '2025-12-16T12:15:35Z', fileType: 'null' },
-  { file: './content/Look at this/Screenshot 2026-01-16 at 15.31.46.png', lastModified: '2026-01-21T20:34:20Z', fileType: 'IMAGE' },
-  { file: './content/Look at this/Screenshot 2026-01-19 at 14.50.43.png', lastModified: '2026-01-21T20:34:20Z', fileType: 'IMAGE' },
-  { file: './content/Look at this/text.txt', lastModified: '2026-01-21T20:34:20Z', fileType: 'TXT' },
-  { file: './content/good day/dolphin.gif', lastModified: '2026-01-21T23:56:50Z', fileType: 'IMAGE' },
-  { file: './content/good day/keyboard.gif', lastModified: '2026-01-21T23:56:50Z', fileType: 'IMAGE' },
-  { file: './content/wassup/1-text.txt', lastModified: '2026-01-21T21:18:31Z', fileType: 'TXT' },
-  { file: './content/wassup/driving-heaven.gif', lastModified: '2026-01-21T21:18:31Z', fileType: 'IMAGE' },
-  { file: './content/wassup/face.gif', lastModified: '2026-01-21T21:18:31Z', fileType: 'IMAGE' },
+  { file: './content/.DS_Store', lastModified: '2026-01-28T12:40:37Z', fileType: 'null' },
+  { file: './content/hello/image.png', lastModified: '2026-01-28T13:08:48Z', fileType: 'IMAGE' },
+  { file: './content/hello/test.txt', lastModified: '2026-01-28T13:08:48Z', fileType: 'TXT' },
 ];
