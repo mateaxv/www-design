8f88a07e69417804fd1c57fa482a7cf175aab673
mateaxv <matea.varvodic@gmail.com>
2026-02-05 13:02:24 +0100

    Video Hinzugefuegt

--- src/modules/render/content.js
+++ src/modules/render/content.js
@@ -4,4 +4,5 @@ export const content = [
   { file: './content/hello/test.txt', lastModified: '2026-01-28T13:08:48Z', fileType: 'TXT' },
   { file: './content/MKGA/image.png', lastModified: '2026-02-05T12:31:35Z', fileType: 'IMAGE' },
   { file: './content/MKGA/text.txt', lastModified: '2026-02-05T12:31:35Z', fileType: 'TXT' },
+  { file: './content/Video/DJI_20250805150950_0101_D.MP4', lastModified: '2026-02-05T12:58:58Z', fileType: 'VIDEO' },
 ];
