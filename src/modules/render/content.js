e70490e63f9dc6dbab78f40a67a878a24fac539a
mateaxv <matea.varvodic@gmail.com>
2026-02-05 12:57:47 +0100

    Audio und video

--- src/modules/render/content.js
+++ src/modules/render/content.js
@@ -2,4 +2,6 @@ export const content = [
   { file: './content/.DS_Store', lastModified: '2026-01-28T12:40:37Z', fileType: 'null' },
   { file: './content/hello/image.png', lastModified: '2026-01-28T13:08:48Z', fileType: 'IMAGE' },
   { file: './content/hello/test.txt', lastModified: '2026-01-28T13:08:48Z', fileType: 'TXT' },
+  { file: './content/MKGA/image.png', lastModified: '2026-02-05T12:31:35Z', fileType: 'IMAGE' },
+  { file: './content/MKGA/text.txt', lastModified: '2026-02-05T12:31:35Z', fileType: 'TXT' },
 ];
